fd44dfd444e6b59880a3fc9cde70ed8487aa1b43
Kaden Seto | 司徒逸希 <127336883+kseto06@users.noreply.github.com>
2024-09-22 23:59:33 -0400

    feat: mini-batch generation, gradient descent

--- framework/model.py
+++ framework/model.py
@@ -1,6 +1,7 @@
 # Class to create a Deep Learning Neural Network model framework, from scratch
 
 import numpy as np #Only np allowed, to speed up computational time for linalg computations
+import math
 
 class Layer:
     def __init__(self):
@@ -42,18 +43,36 @@ def Dense(a_in, W, b, g):
 def ForwardProp(a_in: np.ndarray, params: list, activation: callable, layer: int, cache = dict()):
     # Retrieve the parameters
     W, b = params
+
     # Compute forward prop
     z = np.dot(W, a_in) + b
     a_out = activation(z)
+
     cache[f'Z{layer}'] = z
     cache[f'A{layer}'] = a_out
     cache[f'W{layer}'] = W
     cache[f'b{layer}'] = b
+
     return a_out, cache
 
 # MSE Cost Function
-def MSE():
-    pass
+def MSECost(a_out, Y):
+    '''
+    Compute mean squared error cost function
+
+    Inputs:
+    a_out = output of forward prop (Dense)
+    Y = 'true' labels vector, same shape as output of forward prop
+
+    Output:
+    Cost = loss
+    '''
+    m = a_out.shape[0] #num of training examples
+    cost = 0.
+    for i in range(m):
+        cost += (a_out[i] - Y[i])**2
+
+    return (cost / (2*m))
 
 # Back propagation implementation to get gradients
 def Backprop(a_in, Y, cache):
@@ -70,8 +89,10 @@ def Backprop(a_in, Y, cache):
     gradients = {}
     L = len(cache) // 4 # num of layers
     m = a_in.shape[1] #num of exs
+
     # Initialize the gradient for the last layer
     dA_prev = (1./m * cache[f'A{L}'] - Y)
+
     # Based on backprop computation steps/formulae from Andrew Ng's DL Specialization:
     for layer in reversed(range(1, L+1)):
         #dA_prev represent dA[l-1]
@@ -82,11 +103,62 @@ def Backprop(a_in, Y, cache):
             dW = np.dot(dZ, a_in.T)
         dB = np.sum(dZ, axis=1, keepdims=True)
         dA_prev = np.dot(cache[f'W{layer}'].T, dZ)
+
         #Storing the gradients:
         gradients[f'dZ{layer}'] = dZ
         gradients[f'dW{layer}'] = dW
         gradients[f'dB{layer}'] = dB
+
     return gradients
+
+def generate_minibatches(X, Y, mini_batch_size = 128, seed = 0):
+    '''
+    Generate a list of random minibatches from (X, Y)
+
+    Inputs:
+    X -- input data, shape[input size, num of exs]
+    Y -- true 'label' vector
+
+    m/mini_batch_size mini-batches with full 128 exs
+    final minibatch if there isn't 128 is (m - mini_batch_size * m/mini_batch_size)
+    mini_batch_X = shuffled_X[:, i:j]
+    '''
+    np.random.seed(seed)
+    m = X.shape[1]
+    mini_batches = []
+
+    # Shuffle X, Y
+    permutation = list(np.random.permutation(m))
+    shuffled_X = X[:, permutation]
+    shuffled_Y = Y[:, permutation].reshape((1,m))
+
+    inc = mini_batch_size
+
+    # Creating the mini-batch
+    num_minibatches = math.floor(m / mini_batch_size)
+    for k in range(0, num_minibatches): #Create a counter to count the minibatches without repeating
+        # By formula given above
+        mini_batch_X = shuffled_X[:, k*mini_batch_size : (k+1)*mini_batch_size]
+        mini_batch_Y = shuffled_Y[:, k*mini_batch_size : (k+1)*mini_batch_size]
+        mini_batch = (mini_batch_X, mini_batch_Y) #Grouping
+        mini_batches.append(mini_batch)
+
+    #Handling the case where the last mini-batch may < mini_batch_size
+    if m % mini_batch_size != 0:
+        # Apply the "last batch" formula
+        mini_batch_X = shuffled_X[:, int(m/mini_batch_size)*mini_batch_size:]
+        mini_batch_Y = shuffled_Y[:, int(m/mini_batch_size)*mini_batch_size:]
+        mini_batch = (mini_batch_X, mini_batch_Y) #Grouping
+        mini_batches.append(mini_batch)
+
+    return mini_batches
     
-def Adam():
-    pass
+def gradient_descent(params, gradients, learning_rate = 0.001, num_layers = 3):
+    L = len(params) // num_layers #num of layers in NN's
+
+    # Update rule for each parameter
+    for l in range(1, L+1): #Start from 1 because w and b counts start from 1 (w1,w2...)
+        params[f'W{l}'] = params[f'W{l}'] - learning_rate*gradients[f'dW{l}']
+        params[f'b{l}'] = params[f'b{l}'] - learning_rate*gradients[f'db{l}']
+        
+    return params
