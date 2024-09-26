f3f8076195c3b9fd882dba3de02039760b0f63e7
Kaden Seto | 司徒逸希 <127336883+kseto06@users.noreply.github.com>
2024-09-26 10:48:28 -0400

    fix: Sequential backprop function

--- framework/Sequential.py
+++ framework/Sequential.py
@@ -19,24 +19,25 @@ class Sequential:
         learning_rate: Grad Des learning rate alpha
         cache: List of weight and bias values stored in forward prop
         '''
-        num_layers = 4
+        num_layers = 5
 
         # 1. fwd prop with the current minibatch. Add multiple layers
         # Constructing the neural network:
         a1, cache = ForwardProp(X, params, relu, 1, cache) #Cache empty on the 1st layer
         a2, cache = ForwardProp(a1, params, relu, 2, cache) 
         a3, cache = ForwardProp(a2, params, relu, 3, cache)
-        a4, cache = ForwardProp(a3, params, linear, 4, cache)
+        a4, cache = ForwardProp(a3, params, relu, 4, cache)
+        a5, cache = ForwardProp(a4, params, linear, 5, cache)
 
         # 2. Using the result of the NN (a3) to compute loss with MSE:
-        loss = MSECost(a4, Y)
+        loss = MSECost(a5, Y)
 
         # 3. Apply backprop to find derivative/gradients
-        gradients = Backprop(X, Y, cache)
+        gradients = Backprop(a5, Y, cache)
 
         # 4. Update parameters using Adam
         t = t + 1.
         params, v, s, _, _= Adam(params, gradients, v, s, t, learning_rate, beta1, beta2, epsilon)
 
-        return a4, cache, loss, params, v, s, t
+        return a5, cache, loss, params, v, s, t
             
